318938dd24c3e8ca0d9eb12dbdf9204ae9102866
26rudra <90744578+26rudra@users.noreply.github.com>
2022-11-24 10:17:08 +0530

    Time: 270 ms (63.84%), Space: 14.8 MB (51.37%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        nums.sort()
+        return nums[(n-1)/2]
+            
+            
+            
+        
