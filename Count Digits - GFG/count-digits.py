e7636c66801f3af975bb43087aad1f94b5b274a7
26rudra <90744578+26rudra@users.noreply.github.com>
2022-12-04 12:42:05 +0530

    Added solution - LeetHub

---
+++ Count Digits - GFG/count-digits.py
@@ -0,0 +1,22 @@
+#User function Template for python3
+
+
+class Solution:
+    def evenlyDivides (self, N):
+        return sum([1 for i in str(N) if int(i)!= 0 and N % int(i) == 0])    
+    
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N = int(input())
+       
+
+        ob = Solution()
+        print(ob.evenlyDivides(N))
+# } Driver Code Ends
